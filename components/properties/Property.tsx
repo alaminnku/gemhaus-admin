4cb6a1c80cdb526156e5a3a2b81c321376d6e5c8
Alamin Shaikh <hello@octib.com>
2024-01-08 16:03:47 +0600

    Property page added

--- components/properties/Property.tsx
+++ components/properties/Property.tsx
@@ -1,16 +1,23 @@
 import Error from '@components/layout/Error';
 import { fetchInstance } from '@utils/index';
+import styles from './Property.module.css';
 
 export default async function Property({ id }: { id: string }) {
-  const { data, error } = await fetchInstance(`/properties/${id}`);
+  const { data, error } = await fetchInstance(`/properties/${id}`, {
+    cache: 'no-cache',
+  });
 
   return (
-    <section>
+    <section className={styles.container}>
       {error ? (
         <Error error={error} />
       ) : (
         <>
           <h1>{data.name}</h1>
+          <div
+            className={styles.content}
+            dangerouslySetInnerHTML={{ __html: data.description }}
+          ></div>
         </>
       )}
     </section>
